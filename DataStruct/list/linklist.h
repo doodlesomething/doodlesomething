847ae864f3cc6b8612a8b2ebb0871a01a492672e
doodlesomething <doodlesomething@163.com>
2014-09-11 23:58:07 +0800

    linklist.h

---
+++ DataStruct/list/linklist.h
@@ -0,0 +1,29 @@
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+#define FALSE 0
+#define TRUE 1
+
+typedef int ElemType;
+typedef int Status;
+struct LNode {
+	ElemType data;
+	struct LNode *next;
+};
+typedef struct LNode *LinkList;
+
+
+Status InitList(LinkList *L);
+Status DestoryList(LinkList *L);
+Status ClearList(LinkList L);
+Status ListEmpty(LinkList L);
+int ListLength(LinkList L);
+Status GetElem(LinkList L,int i,ElemType *e);
+int LocateElem(LinkList L,int e);
+Status PreElem(LinkList L,ElemType cur_e,ElemType *pre_e);
+Status NextElem(LinkList L,ElemType cur_e,ElemType *next_e);
+Status ListInsert(LinkList L,int i,ElemType e);
+Status ListDelete(LinkList L,int i,ElemType *e);
+Status ListTraverse(LinkList L,void (*vist)(ElemType));
+void CreateList(LinkList *L,int n);
+void CreateListReverse(LinkList *L,int n);
