1e644abff94e81cf3f16c8e50e9d1947385a06d7
doodlesomething <doodlesomething@163.com>
2014-09-17 22:07:05 +0800

    SLinkList.c

--- DataStruct/list/SLinkList.c
+++ DataStruct/list/SLinkList.c
@@ -1,15 +1,18 @@
-/*---------------------------------------------------------------------------------------
+/*---------------------------------------------------------------------------------------------
 	* file:SLinkList.c
 	* date:9-17-2014
 	* author:doodlesomething@163.com
 	* version:1.0
-	* description:静态链表的实现，静态链表主要用在没有
-	指针的高级语言中，使其可以使用链表结构
-	* more : 
+	* description:静态链表的实现，静态链表主要用在没有指针的高级语言中，使其可以使用链表结构
+	* more : 数组的元素都是由两个数据域组成, 也钮 和 cur 。也就是说,数组的每个
+	下标都对应一个 也ta 和一个 cur o 数据域也 ta ,用来存放数据元素, 也就是通常我
+	们要处理的数据;而游标 cur 相当于单链表中的 next 指针,存放该元素的后继在数组
+	中的下标。在静态链表中,操作的是如且,不存在像动态链表的结点申请和释放问题,所以
+	我们需要自己实现这两个 函数,才可以做插入和删除的操作
 	* somethingelse: 1.L[MAXSIZE-1] 相当于链表头结点,当链表为空时L[MAXSIZE-1].cur等于0
 		  当链表不为空时其L[MAXSIZE-1].cur等于1-->即其始终指向第一个节点
 		  2.所谓的备用链表即原数组中空闲节点，只是方便理解罢了
-------------------------------------------------------------------------------------------*/
+----------------------------------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,14 +21,22 @@
 
 int main(int argc,char *argv[]) {
 	SLinkList L;
-	int i;
+	int i,pre_e,next_e,elem;
 	InitList(L);
 
-	for(i=0;i < 10;i++)
+	for(i=1;i < 6;i++)
 		ListInsert(L,1,i);
 
 	ListTraverse(L,visit);
-
+	printf("\n");
+	GetElem(L,5,&elem);
+	printf("Length:%d, GetElem:%d ,LocateElem:%d \n",ListLength(L),elem,LocateElem(L,5));
+	PreElem(L,2,&pre_e);
+	NextElem(L,2,&next_e);
+	printf("PreElem:%d, NextElem:%d ,IsEempty:%d\n",pre_e,next_e,ListEmpty(L));
+	ListDelete(L,2);
+	printf("Length:%d\n",ListLength(L));
+	ListTraverse(L,visit);
 	printf("\n");
 }
 
@@ -44,6 +55,29 @@ void InitList(SLinkList L) {
 	
 	//最后一个节点的cur值为0
 	L[MAXSIZE-1].cur = 0;
+}
+
+
+/*
+* @description 销毁链表
+* @param SLinkList L
+* @return void
+*/
+void DestoryList(SLinkList L) {
+	int i;
+
+	for(i = 0; i < MAXSIZE - 1; i++)
+		L[i].cur = 0;
+}
+
+
+/*
+* @description 清空链表
+* @param SLinkList L
+* @return void
+*/
+void ClearList(SLinkList L) {
+	InitList(L);	
 }
 
 
@@ -87,6 +121,8 @@ int ListLength(SLinkList L) {
 }
 
 
+
+
 /*
 * @description 将元素插入静态链表第i个元素前即使表是空的,当输入非法或无空闲节点是返回ERROR
 * @param SLinkList L
@@ -123,6 +159,104 @@ Status ListInsert(SLinkList L,int i,int elem) {
 	return ERROR;
 }
 
+/*
+* @description 有elem返回L中第i个元素的值
+* @param SLinkList L
+* @param int i
+* @param ElemType *elem
+* @return Status
+*/
+Status GetElem(SLinkList L,int i,ElemType *elem) {
+	int k,j;
+	
+	if( i < 1 || i > ListLength(L) + 1)
+		return ERROR;
+	j = 1;
+	k = L[MAXSIZE-1].cur;
+
+	while(k) {
+		if(i == j) {
+			*elem = L[k].data;
+			return OK;
+		}
+		k = L[k].cur;
+		j++;
+	}
+
+	return ERROR;
+}
+
+
+/*
+* @description 返回指定元素的位置
+* @param SLinkList L
+* @param ElemType elem
+* @return int i
+*/
+int LocateElem(SLinkList L,ElemType elem) {
+	int k,i;
+
+	k = L[MAXSIZE-1].cur;
+	i = 1;
+
+	while(k) {
+		if(L[k].data == elem)
+			return i;
+		k = L[k].cur;
+		i++;
+	}
+
+	return 0;
+}
+
+/*
+* @description 获取某个指定元素(不是第一个)的前驱元素
+* @param SLinkList L
+* @param ElemType elem
+* @param ElemType *pre_e
+* @return Status
+*/
+Status PreElem(SLinkList L,ElemType elem,ElemType *pre_e) {
+	int k,j;
+
+	k = L[MAXSIZE-1].cur;
+
+	while(k) {
+		j = L[k].cur;
+		if(j  && (L[j].data == elem) ) {
+			*pre_e = L[k].data;
+			return OK;
+		}
+
+		k = L[k].cur;
+	}
+	return ERROR;
+}
+
+/*
+* @description 返回某个指定元素(非最后一个)的后继元素
+* @param SLinkList L
+* @param ElemType elem
+* @param ElemType *next_e
+* @return void
+*/
+Status NextElem(SLinkList L,ElemType elem,ElemType *next_e) {
+	int k,j;
+
+	k = L[MAXSIZE-1].cur;
+
+	while(k) {
+		j = L[k].cur;
+		if(j && (L[k].data == elem) ) {
+			*next_e = L[j].data;
+			return OK;
+		}
+		k = L[k].cur;
+	}
+	return ERROR;
+}
+
+
 
 /*
 * 将空闲节点放到备用链表上
@@ -132,6 +266,15 @@ Status ListInsert(SLinkList L,int i,int elem) {
 void Free_SL(SLinkList L,int k) {
 	L[k].cur = L[0].cur;
 	L[0].cur = k;
+}
+
+/*
+* @description 判空
+* @param SLinkList L
+* @return Status
+*/
+Status ListEmpty(SLinkList L) {
+	return L[MAXSIZE-1].cur ? FALSE : TRUE ;
 }
 
 
@@ -155,7 +298,8 @@ Status ListDelete(SLinkList L,int i) {
 
 	j = L[k].cur;
 	L[k].cur = L[j].cur;
-	Free_SL(L,k);
+
+	Free_SL(L,j);
 
 	return OK;
 }
