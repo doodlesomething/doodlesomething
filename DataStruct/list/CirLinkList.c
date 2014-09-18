a268e2d351f0216f2dbaf56bf90d2e7f8807c3f2
doodlesomething <doodlesomething@163.com>
2014-09-18 22:00:02 +0800

    CirLinkList.c

--- DataStruct/list/CirLinkList.c
+++ DataStruct/list/CirLinkList.c
@@ -9,21 +9,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "cirlinklist.h"
+#include "circulalist.h"
 
 int main(int argc,char *argv[]) {
 	LinkList La,Lb,ra,rb;
-	char head_e,last_e;
+	char head_e,last_e,del_e;
+	char c='b';
+	int len;
+
 	ra = CreateList(&La);
 	rb = CreateList(&Lb);
 	ListTraverse(La,visit);
 	printf("\n");
 	ListTraverse(Lb,visit);
-	rb = MergeList(ra,rb);
-	ListTraverse(La,visit);
 	GetHead(ra,&head_e);
 	GetLast(ra,&last_e);
 	printf("\n head_e:%c, last_e:%c\n",head_e,last_e);
+	rb = MergeList(ra,rb);
+	ListTraverse(La,visit);
+	printf("\n length:%d\n",ListLength(La));
+	ListInsert(La,2,c);
+	ListTraverse(La,visit);
+	printf("\n");
+	ListDelete(La,ListLength(La),&del_e);
+	ListTraverse(La,visit);
+	printf("%c\n",del_e);
 }
 
 /*
@@ -133,5 +143,87 @@ LinkList MergeList(LinkList rearA,LinkList rearB) {
 	free(q);
 
 	return rearB;
+}
+
+/*
+* @description 返回链表长度
+* @param LinkList L
+* @return len
+*/
+int ListLength(LinkList L) {
+	int len;
+	LinkList p;
+
+	len = 0;
+	p = L->next;
+
+	while(p != L && p->data != '\n') {
+		len++;
+		p = p->next;
+	}
+
+	return len;
+}
+
+/*
+* @description 在第i个节点前插入元素elem
+* @param LinkList L
+* @param int i
+* @param ElemType elem
+* @return Status
+*/
+Status ListInsert(LinkList L,int i,ElemType elem) {
+	LinkList p,tmp,q;
+	int j;
+	
+	j = 1;
+	p = L;
+
+	if(i < 1 || i > ListLength(L) + 1)
+		return ERROR;
+	
+	while(j < i) {
+		p = p->next;
+		j++;
+	}
+
+	q = (LinkList) malloc(sizeof(struct LNode));
+	q->data = elem;
+	tmp = p->next;
+	p->next = q;
+	q->next = tmp;
+
+	return OK;
+}
+
+
+/*
+* @description 删除链表中第i个元素,并通过elem返回
+* @param LinkList L
+* @param int i
+* @param ElemType *elem
+* @return Status
+*/
+Status ListDelete(LinkList L,int i,ElemType *elem) {
+	LinkList p,tmp;
+	int j;
+
+	p = L;
+	j = 1;
+
+	if(i < 1 || i > ListLength(L))
+		return ERROR;
+	
+	while(j < i) {
+		p = p->next;
+		j++;
+	}
+	
+	tmp = p->next;
+	*elem = tmp->data;
+	p->next = tmp->next;
+	free(tmp);
+	
+	return OK;
 }
 
