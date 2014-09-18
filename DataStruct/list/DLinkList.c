c18efa9397231241c8549d4907ac1fe044357848
doodlesomething <doodlesomething@163.com>
2014-09-18 22:48:17 +0800

    DLinkList.c

---
+++ DataStruct/list/DLinkList.c
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------
+	* file:DLinkList.c
+	* date:9-18-2014
+	* author:doodlesomething@163.com
+	* version:1.0
+	* description:双向链表的实现
+------------------------------------------------------------------------------*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dlinklist.h"
+
+int main(int argc,char *argv[]) {
+
+	return 0;
+}
