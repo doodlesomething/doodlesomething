2996827718f6d5134be7e41ad5bf8bc382e83467
doodlesomething <doodlesomething@163.com>
2014-09-21 11:16:51 +0800

    CirList/

---
+++ DataStruct/list/CirList/cirlinklist.h
@@ -0,0 +1,34 @@
+
+/*--------------------------------------------------------------------
+	* file:circulalist.h -->head file for CirculaList.c 
+	* date: 9-18-2014
+	* author: doodlesomething@163.com
+	* version:1.0
+	* description:循环单链表的实现
+---------------------------------------------------------------------*/
+
+#define FLASE 0
+#define TRUE 1
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+
+typedef char ElemType;
+typedef int Status;
+
+struct LNode {
+	ElemType data;
+	struct LNode *next;
+};
+
+typedef struct LNode *LinkList;
+
+LinkList CreateList(LinkList *L);
+void ListTraverse(LinkList L,void (*visit) (ElemType));
+void visit(ElemType elem);
+Status GetHead(LinkList rear,ElemType *elem);
+Status GetLast(LinkList rear,ElemType *elem);
+LinkList MergeList(LinkList rearA,LinkList rearB);
+int ListLength(LinkList L);
+Status ListInsert(LinkList L,int i,ElemType elem);
+Status ListDelete(LinkList L,int i,ElemType *elem);
