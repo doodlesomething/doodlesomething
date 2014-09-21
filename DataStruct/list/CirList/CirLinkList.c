f63cb4263354e5bc67febbd5a683923af491b076
doodlesomething <doodlesomething@163.com>
2014-09-21 18:13:07 +0800

    CirLinkList.c

--- DataStruct/list/CirList/CirLinkList.c
+++ DataStruct/list/CirList/CirLinkList.c
@@ -9,35 +9,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "circulalist.h"
+#include "cirlinklist.h"
 
-int main(int argc,char *argv[]) {
-	LinkList La,Lb,ra,rb;
-	char head_e,last_e,del_e;
-	char c='b';
-	int len;
 
-	ra = CreateList(&La);
-	rb = CreateList(&Lb);
-	ListTraverse(La,visit);
-	printf("\n");
-	ListTraverse(Lb,visit);
-	GetHead(ra,&head_e);
-	GetLast(ra,&last_e);
-	printf("\n head_e:%c, last_e:%c\n",head_e,last_e);
-	rb = MergeList(ra,rb);
-	ListTraverse(La,visit);
-	printf("\n length:%d\n",ListLength(La));
-	ListInsert(La,2,c);
-	ListTraverse(La,visit);
-	printf("\n");
-	ListDelete(La,ListLength(La),&del_e);
-	ListTraverse(La,visit);
-	printf("%c\n",del_e);
-}
+
 
 /*
-* @description:创建带头结点和尾指针的单链表
+* @description:创建带头
+
+结点和尾指针的单链表
 * @param LinkList *L
 * @return LinkList r
 */
