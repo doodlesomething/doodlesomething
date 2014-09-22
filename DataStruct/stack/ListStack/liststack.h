129c78f58d4d2dbc4b4d640855c0bcb5fbc1ef3d
doodlesomething <doodlesomething@163.com>
2014-09-22 23:10:29 +0800

    liststack.h

---
+++ DataStruct/stack/ListStack/liststack.h
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------
+	*file:ListStack.c
+	*date:22-9-2014
+	*author:doodlesomething@163.com
+	*version:1.0
+	*description:链栈的基本操作
+----------------------------------------------------------------*/
+
+#define TRUE 1
+#define ERROR 0
+#define 
