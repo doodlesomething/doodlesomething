e38b92acfc60ef7b5e632ecce9a1728c0738f5e8
doodlesomething <doodlesomething@163.com>
2014-09-26 00:36:38 +0800

    simulation.h

---
+++ DataStruct/queue/Simulation/simulation.h
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------
+	* file:simulation.h -->head file for Simulation.c
+	* date:25-9-2014
+	* author:doodlesomething@163.com
+	* version:.10
+	* description:银行离散事件模拟
+-----------------------------------------------------------*/
+
+
+
