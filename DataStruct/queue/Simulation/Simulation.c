aee3bedd6d16de06a6585b4da139eb938c087f58
doodlesomething <doodlesomething@163.com>
2014-09-26 00:36:25 +0800

    Simulation.c

---
+++ DataStruct/queue/Simulation/Simulation.c
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------
+	* file:Simulation.c
+	* date:25-9-2014
+	* author:doodlesomething@163.com
+	* version:.10
+	* description:银行离散事件模拟
+-----------------------------------------------------------*/
+
+
+#include <stdio.h>
+#include "linklist.h"
+#include "linkqueue.h"
+
+int main(int argc,char *argv[]) {
+	
+
+	exit(0);
+}
