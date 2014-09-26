1b4fa7f5fd0361ba4e6b8817f07244d9133a6124
doodlesomething <doodlesomething@163.com>
2014-09-26 22:19:04 +0800

    test.c

--- DataStruct/queue/LinkQueue/test.c
+++ DataStruct/queue/LinkQueue/test.c
@@ -9,17 +9,31 @@
 #include <stdio.h>
 #include "linkqueue.h"
 
-int main(int argc,char *argv[]) {
-	LinkQueue Q;
+int main() {
+	
+	LinkQueue Q,Q1;
 	int i;
 
 	InitQueue(&Q);
+	InitQueue(&Q1);
+
 	for(i = 0; i < 6;i++)
 		EnQueue(&Q,i);
-	
+	Push(&Q,&Q1,7);
 	QueueTraverse(Q,visit);
-	printf("\nLength:%d,Empty:%d\n",QueueLength(Q),QueueEmpty(Q));
+	printf("\n%d\n",Pop(&Q,&Q1));
+	QueueTraverse(Q,visit);
 
+	printf("\nLength:%d,Empty:%d\n",QueueLength(Q),QueueEmpty(Q));
+	
+
+	/*
+	测试用例结果：
+	0 1 2 3 4 5 7
+	0 1 2 3 4 5 
+	7 
+	6 0
+	*/
 }
 
 
