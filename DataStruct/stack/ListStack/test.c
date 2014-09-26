13d2cc93f8a3d0a4950f591fa821eeb63a5cc6a9
doodlesomething <doodlesomething@163.com>
2014-09-26 19:18:53 +0800

    test.c

--- DataStruct/stack/ListStack/test.c
+++ DataStruct/stack/ListStack/test.c
@@ -11,32 +11,31 @@
 #include <stdio.h>
 
 int main(int argc,char *argv[]) {
-	LinkStack S;
+	LinkStack S,S1;
 	int i;
 	int pop_e,top_e;
 
 	InitStack(&S);
+	InitStack(&S1);
 	for(i = 1; i < 6; i++) {
 		Push(&S,i);
 	}
 	StackTraverse(S,visit);
 	printf("\n");
 
-	printf("StackEmpty:%d, StackLength:%d\n",StackEmpty(S),StackLength(S));
+	InsertQueue(&S,6);
+	InsertQueue(&S,7);
+	InsertQueue(&S,8);
+	StackTraverse(S,visit);
+	printf("\n%d\n",DeleQueue(&S,&S1));
+	StackTraverse(S,visit);
 
-	Pop(&S,&pop_e);
-	printf("pop_e:%d\n",pop_e);
-	GetTop(S,&top_e);
-	printf("top_e:%d\n",top_e);
-	ClearStack(&S);
-	printf("StackEmpty:%d, StackLength:%d\n",StackEmpty(S),StackLength(S));
-	/*测试用例结果：
-	5	4	3	2	1	
-	StackEmpty:0, StackLength:5
-	pop_e:5
-	top_e:4
-	StackEmpty:1, StackLength:0
 
+	/*
+	测试用例结果：
+	5 4 3 2 1
+	8 7 6 5 4 3 2 1
+	1
 	*/
 
 }
