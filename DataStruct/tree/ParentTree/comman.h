dfb6e9f28829e6168159f7caf1c3ee8a00010ee3
doodlesomething <doodlesomething@163.com>
2014-10-06 16:55:07 +0800

    comman.h

---
+++ DataStruct/tree/ParentTree/comman.h
@@ -0,0 +1,22 @@
+
+#ifndef COM
+#define COM comman.h
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+
+
+typedef char TElemType;
+
+typedef struct {
+	TElemType name;
+	int num;
+}QElemType;
+
+typedef int Status;
+
+
+#endif
