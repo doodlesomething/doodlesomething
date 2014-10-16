4aa0963298df4865d822287e97e306dbde72b3b9
doodlesomething <doodlesomething@163.com>
2014-10-16 17:04:01 +0800

    test.c

--- DataStruct/graph/ALGraph/test.c
+++ DataStruct/graph/ALGraph/test.c
@@ -20,4 +20,24 @@ void main() {
 	printf("\n");
 	DFSTraverse(G,PrintElem);
 	printf("\n");
+
+
+	/*
+	测试用例结果：
+	please enter the kind of graph(DG:0,DN:1,UDG:2,UDN:3):2
+	please the vexnum and arcnum:8,8
+	please enter the value of each vertex:1,2,3,4,5,6,7,8
+	please enter the heads and tails:
+	1,2
+	1,3
+	2,4
+	2,5
+	3,6
+	3,7
+	4,8
+	5,8
+	12345678
+	12485367
+
+	*/
 }
