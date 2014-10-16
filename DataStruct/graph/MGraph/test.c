4ef61f7e71410e7c8ad659145a25fe5075288004
doodlesomething <doodlesomething@163.com>
2014-10-16 17:03:43 +0800

    test.c

--- DataStruct/graph/MGraph/test.c
+++ DataStruct/graph/MGraph/test.c
@@ -21,4 +21,24 @@ int main(int argc,char *argv[]) {
 	printf("\n");
 	BFSTraverse(G,PrintElem);
 	printf("\n");
+
+
+	/*
+	测试用例结果：
+	please enter the kind of the graph(DG:0,DN:1,UDG:2,UDN:3):2
+	please enter vexnum, arcnum is info(1 or 0):8,8,0
+	the value of each vertex:1,2,3,4,5,6,7,8
+	please 8 heads and 8 tails:
+	1,2
+	1,3
+	2,4
+	2,5
+	3,6
+	3,7
+	4,8 
+	5,8
+	12485367
+	12345678
+
+	*/
 }
